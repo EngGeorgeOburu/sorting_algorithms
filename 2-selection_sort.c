10ebc59406e4a8ff839dfa761d0192b83f710d84
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-02-19 15:29:56 +0300

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,17 +8,18 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
 	size_t i, j, min_idx;
 	int temp;
+
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if array[j] < array[min_idx])
+			if (array[j] < array[min_idx])
 				min_idx = j;
 		}
 		if (min_idx != i)

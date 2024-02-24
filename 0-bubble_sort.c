cccc64f12d268acb2fc409c5d1e1d94b90065814
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-02-25 02:52:45 +0300

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,32 +7,32 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    unsigned int i, j;
-    int temp;
-    int swapped;
+       	unsigned int i, j;
+	int temp;
+	int swapped;
 
-    /* Outer loop to traverse throught the entire array*/
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0; /* Flag to track if any swap occurs in the inner loop */
-            /* Inner loop comparing adjacent elements and swap if necessary */
-            for(j = 0; j < size - i -1; j++)
-            {
-                /* Check if the current element is greater than the next elelmtn */
-                if (array[j] > array[j + 1])
-                {
-                        /*swap the elements */
-                        temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
-                        swapped = 1; /*set swapped flag to true */
-                        print_array(array, size);
-                }
-            }
-        /* If no two elements are swapped in the inner array, then it's sorted */
-        if (swapped == 0)
-            break;
-    }
+	/* Outer loop to traverse throught the entire array*/
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0; /* Flag to track if any swap occurs in the inner loop */
+		/* Inner loop comparing adjacent elements and swap if necessary */
+		for(j = 0; j < size - i -1; j++)
+		{
+			/* Check if the current element is greater than the next elelmtn */
+			if (array[j] > array[j + 1])
+			{
+				/*swap the elements */
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1; /*set swapped flag to true */
+				print_array(array, size);
+			}
+		}
+		/* If no two elements are swapped in the inner array, then it's sorted */
+		if (swapped == 0)
+			break;
+	}
 }
                         
 
--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -14,7 +14,7 @@ void swap(int *a, int *b)
 	*b = temp;
 }
 /**
- * shell_sort - Sorts an array of integers in ascending order uisng
+ * Shell_sort - Sorts an array of integers in ascending order uisng
  * 		the shell sort algorithm with the Knuth sequence
  * @array: The array to be sorted
  * @size: The size of the array
